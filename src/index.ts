765200d7d37f28cf3a237a7fdb560aae45e88858
Joel Gustafson <joelg@mit.edu>
2021-01-29 11:46:14 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * as rdf from "./rdf.js"
+export * as xsd from "./xsd.js"
+export * as ul from "./ul.js"
---
+++ src/rdf.ts
@@ -0,0 +1,6 @@
+export const type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+	nil = "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil",
+	first = "http://www.w3.org/1999/02/22-rdf-syntax-ns#first",
+	rest = "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest",
+	langString = "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString",
+	JSON = "http://www.w3.org/1999/02/22-rdf-syntax-ns#JSON"
